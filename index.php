595e6b47fc1e84c3f552317b855f992edfd34182
tomn94 <tmnstudios@hotmail.fr>
2016-12-03 20:24:58 +0800

    + favicons & browser customization

--- index.php
+++ index.php
@@ -11,6 +11,24 @@
     <title><?php echo $PAGE_TITLE; ?></title>
     <link rel="stylesheet" href="style.css">
     <script src="script.js"></script>
+    
+    <?php /* Browser customization */ ?>
+    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="apple-touch-icon-precomposed" href="favicon.png">
+    <link rel="icon" sizes="256x256" href="favicon.png">
+    <meta name="msapplication-TileImage" content="favicon.png"/>
+    <meta name="msapplication-TileColor" content="#F1783D"/>
+    <meta name="application-name" content="<?php echo $PAGE_TITLE_SHORT; ?>">
+    <meta name="apple-mobile-web-app-title" content="<?php echo $PAGE_TITLE; ?>">
+    <meta name="theme-color" content="#F1783D">
+    <link color="#F1783D" href="favicon.svg" rel="mask-icon">
+    
+    <?php /* Bots integration */ ?>
+    <meta property="og:title" content="<?php echo $PAGE_TITLE; ?>" />
+    <meta property="og:image" content="favicon.png" />
+    <meta property="og:image:type" content="image/png" />
+    <meta property="og:image:width" content="256" />
+    <meta property="og:image:height" content="256" />
   </head>
   <body>
       
