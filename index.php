cb4756c1e62c2578cf7bce643e188fc14d9de88a
tomn94 <tmnstudios@hotmail.fr>
2016-12-03 19:24:09 +0800

    + Base structure

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+  
+  $PAGE_TITLE = "Thomas Naudet — Student Engineer+Manager";
+  $PAGE_TITLE_SHORT = "Thomas Naudet";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title><?php echo $PAGE_TITLE; ?></title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+  </head>
+  <body>
+      
+  </body>
+</html>
