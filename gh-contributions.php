82eb056090934d0f13f8731d04283997de0b05bb
tomn94 <tmnstudios@hotmail.fr>
2016-12-05 23:19:28 +0800

    ~ Strip eventual tags from GitHub

--- gh-contributions.php
+++ gh-contributions.php
@@ -5,6 +5,6 @@ $res = file_get_contents("https://github.com/tomn94");
 preg_match("/([0-9]+) contributions/i", $res, $match);
 
 if (count($match) > 1)
-    echo htmlspecialchars($match[1]);
+    echo strip_tags(htmlspecialchars($match[1]));
 else
     echo "So many";
