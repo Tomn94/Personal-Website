fe4eaa82ae0eee0185ad54bafec544986f7d4570
tomn94 <tmnstudios@hotmail.fr>
2017-05-14 05:20:25 +0800

    ~ Let’s be precise in WWDC paragraph

--- data.php
+++ data.php
@@ -88,7 +88,7 @@ $PROJECTS =
     [
         ["title"   => "Apple WWDC’17 Scholarship Winner",
          "id"      => "wwdc",
-         "details" => "Swift Playgrounds Book introducing myself for Apple Worldwide Developers Conference 2017 Scholarship.<br>&nbsp;</br>Selected among 350 students in the world, I was indeed invited to attend the week of conferences among 5,000 engineers and developers.<br>&nbsp;</br>The Playgrounds Book is interactive: you can play with the Swift code on the left, and experience the result on the right.",
+         "details" => "Swift Playgrounds Book introducing myself for Apple Worldwide Developers Conference 2017 Scholarship.<br>&nbsp;</br>Selected with 350 other students in the world, I was indeed invited to attend the week of conferences among 5,000 engineers and developers.<br>&nbsp;</br>The Playgrounds Book is interactive: you can play with the Swift code on the left, and experience the result on the right.",
          "date"    => "Spring 2017",
          "imgs"    => ["img/projects/wwdc.png"],
          "links"   => [["View &amp; Download on GitHub", "https://github.com/Tomn94/WWDC-2017-Scholarship"],
